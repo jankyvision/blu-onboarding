980a28d9fccdbff621b06cee892646e4fc6e1df4
jankyvision <33376008+jankyvision@users.noreply.github.com>
2019-12-03 16:19:35 -0800

    dont need

--- script.js
+++ script.js
@@ -157,12 +157,3 @@ $(function () {
 });
 
 
-
-var $tabs = $('.tabs-wrapper li');
-$('#prevtab').on('click', function() {
-    $tabs.filter('.active').prev('li').find('a[data-toggle="tab"]').tab('show');
-});
-$('#nexttab').on('click', function() {
-    $tabs.filter('.active').next('li').find('a[data-toggle="tab"]').tab('show');
-});
-
